6909a0517ddcb44b8b0d7aed1fbf7ba79d434f00
tcs-dsharma <dsharma@containerstore.com>
2022-10-03 16:11:46 +0530

    Content Asset Learned

---
+++ LWC Certification Udemy/force-app/main/default/lwc/contentAssetExample/contentAssetExample.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import STG_MGT_PHOTO from '@salesforce/contentAssetUrl/Store_Leadership_Update_42522docx';
+export default class ContentAssetExample extends LightningElement {
+
+    file = STG_MGT_PHOTO;
+}
